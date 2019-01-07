3eee09450434c0c80f074a67af320dbd6f61efc5
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2019-01-07 22:46:35 +0900

    Fix error

--- cli-init.js
+++ cli-init.js
@@ -11,8 +11,6 @@ fs.readFile(CONFIG_FILE_PATH, (err, data) => {
         console.log('success!')
         console.log('Please rewrite them for your organizations.')
         console.log('path: ' + CONFIG_FILE_PATH)
-        console.log('---')
-        console.log(data.toString('utf-8'))
       }
     })
   } else {
